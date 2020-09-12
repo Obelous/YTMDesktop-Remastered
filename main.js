058f3eab87c9a358c78e8307684789b9803af9da
Casper S <casper@caspers.club>
2020-09-12 19:00:22 +0200

    test alerts

--- main.js
+++ main.js
@@ -29,10 +29,10 @@ const touchService = new MediaService();
 touchService.startService();
 touchService.on('play', () => dialog.showMessageBox(null));
 touchService.on('pause', () => dialog.showMessageBox(null));
-/*touchService.setMetaData({
-  title: 'Never Gonna Give You Up',
+touchService.setMetaData({
+  title: 'Never Gonna Give You Up'
   // Other track meta data here
-});*/
+});
 
 touchService.on('seek', (to) => {
   dialog.showMessageBox(null)
